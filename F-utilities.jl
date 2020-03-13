b1c7f22b524b353c344f02563ac74a020a89470c
Jacopo Surace <jacopo.surace@gmail.com>
2020-03-13 19:36:28 +0100

    Aggiornamenti

--- F-utilities.jl
+++ F-utilities.jl
@@ -268,6 +268,72 @@ function Contour(Γ)
 end
 
 
+function GS_gamma(D,U)
+   N = div(size(D,1),2);
+
+   Gamma_diag_base = zeros(Complex{Float64}, 2*N, 2*N);
+   for iiter=1:N
+       Gamma_diag_base[iiter+N, iiter+N] = 1;
+   end
+   Gamma = U*Gamma_diag_base*U';
+
+   Gamma = (Gamma+(Gamma'))/2.
+   return Gamma;
+end
+
+
+function Termal_fix_beta(Diag_H, U_H, beta)
+ N_f   = convert(Int64, size(Diag_H,1)/2.);
+
+ gamma = zeros(Complex{Float64}, size(Diag_H,1),size(Diag_H,1))
+ for kiter=1:N_f
+   e_k = Diag_H[kiter+N_f,kiter+N_f];
+   gamma[kiter,kiter] = 1/(1+exp(beta*e_k));
+   gamma[kiter+N_f,kiter+N_f] = 1/(1+exp(-beta*e_k))
+ end
+
+ gamma = U_H*gamma*U_H';
+
+ return gamma;
+end
+
+
+
+function Thermal_fix_energy(Diag_H, U_H, conserved_energy)
+ N_f   = convert(Int64, size(Diag_H,1)/2.);
+ a     = 0;
+ b     = 100;
+ beta = 0;
+
+
+ temp_energy = 0;
+ for iiter=1:1000
+     beta  = (a+b)/2;
+     temp_energy = 0
+     for kiter=1:N_f
+       e_k = Diag_H[kiter+N_f,kiter+N_f];
+       temp_energy += e_k*(1/(1+exp(beta*e_k))-1/(1+exp(-beta*e_k)));
+     end
+     if (temp_energy > conserved_energy)
+       a = beta;
+       else
+       b = beta;
+     end
+ end
+
+ println("Thermal state with energy precision of:", abs(temp_energy-conserved_energy)," and beta: ", beta);
+
+ gamma = zeros(Complex{Float64}, size(Diag_H,1),size(Diag_H,1))
+ for kiter=1:N_f
+   e_k = Diag_H[kiter+N_f,kiter+N_f];
+   gamma[kiter,kiter] = 1/(1+exp(beta*e_k));
+   gamma[kiter+N_f,kiter+N_f] = 1/(1+exp(-beta*e_k))
+ end
+
+ gamma = U_H*gamma*U_H';
+
+ return gamma, β;
+end
 
 ###################################################################################
 
