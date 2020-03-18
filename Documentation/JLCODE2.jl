9b997a2eb9c13af87c3b2bb42ecd3498b75464a1
Jacupo <Jacupo@users.noreply.github.com>
2020-03-18 19:05:54 +0100

    Prova 2

--- Documentation/JLCODE2.jl
+++ Documentation/JLCODE2.jl
@@ -11,8 +11,3 @@ H_B = Random_NNhamiltonian(N_A);
 Γ_A = GS_Gamma(HA_D, UA_D);
 Energy_fermions(Γ_A,HA_D, UA_D)
 Γ_B = Thermal_fix_beta(Diag_h(H_B),1)
-Γ_B, β, Δ = Thermal_fix_energy(Diag_h(H_B),Energy(Γ_B,Diag_h(H_B)))
-Γ_B, β, Δ = Thermal_fix_energy(Diag_h(H_B),Energy(Γ_B,Diag_h(H_B)))
-
-#Diagonalise the hamiltonian
-H_D, U_D = Diag_h(H)
